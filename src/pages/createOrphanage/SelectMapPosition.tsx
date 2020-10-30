ebb5ef1a347572a638d16b10d382a8ae6dc7a10b
EduardoAraD <araujocarlos893@gmail.com>
2020-10-30 11:53:25 -0300

    create SelectMapPosition

---
+++ src/pages/createOrphanage/SelectMapPosition.tsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, Dimensions, Text } from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+import MapView, { MapEvent, Marker } from 'react-native-maps';
+
+import mapMarkerImg from '../../images/map-marker.png';
+import { useApp } from '../../contexts/store'
+
+export default function SelectMapPosition() {
+  const navigation = useNavigation();
+  const { latitude, longitude } = useApp().position;
+
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0 })
+
+  function handleNextStep() {
+    navigation.navigate('OrphanageData', { position });
+  }
+
+  function handleSelectMapPosition(event: MapEvent) {
+    setPosition(event.nativeEvent.coordinate);
+  }
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        initialRegion={{
+          latitude: latitude,
+          longitude: longitude,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+        style={styles.mapStyle}
+        onPress={handleSelectMapPosition}
+      >
+        {position.latitude !== 0 && (
+          <Marker
+            icon={mapMarkerImg}
+            coordinate={{ latitude: position.latitude, longitude: position.longitude }}
+          />
+        )}
+      </MapView>
+
+      {position.latitude !== 0 && (
+        <RectButton style={styles.nextButton} onPress={handleNextStep}>
+          <Text style={styles.nextButtonText}>Próximo</Text>
+        </RectButton>
+      )}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: 'relative'
+  },
+
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+
+  nextButton: {
+    backgroundColor: '#15c3d6',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 56,
+
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 40,
+  },
+
+  nextButtonText: {
+    fontFamily: 'Nunito_800ExtraBold',
+    fontSize: 16,
+    color: '#FFF',
+  },
+
+  intro: {
+    backgroundColor: 'rgba(30, 185, 225, 0.4)',
+    paddingHorizontal: 40,
+    paddingVertical: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  textIntro: {
+    fontSize: 24,
+    lineHeight: 34,
+    fontFamily: 'Nunito_800ExtraBold',
+    color: '#FFFFFF'
+  }
+})
